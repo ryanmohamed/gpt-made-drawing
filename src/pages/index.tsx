cfcaa384c91adcab14dbaa5a93463b682d50cfe0
Ryan <reyaznyc@gmail.com>
2023-07-20 13:52:26 -0400

    image trace

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,6 +36,17 @@ export default function Home() {
     setSelectedDrawing(drawing);
   };
 
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setSelectedDrawing(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
       <h1 className="text-4xl font-bold underline my-10">Draw App</h1>
@@ -43,6 +54,14 @@ export default function Home() {
         <ColorPicker color={color} setColor={setColor} />
         <button className="p-2 bg-gray-700 rounded text-white mx-2" onClick={handleReset}>Reset</button>
         <button className="p-2 bg-gray-700 rounded text-white mx-2" onClick={handleSave}>Save</button>
+      </div>
+      <div className="mb-4 flex justify-between items-center bg-stone-800 text-white p-4 rounded">
+          <label className="text-gray-100 mr-2">Upload image:</label>
+          <input 
+            type="file" 
+            className="text-gray-100 cursor-pointer" 
+            onChange={handleFileChange} 
+          />
       </div>
       <CanvasWithNoSSR color={color} reset={reset} setReset={setReset} selectedImage={selectedDrawing} />
       <div className="m-10 flex flex-wrap justify-center">
